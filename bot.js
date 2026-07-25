790769b390eb128663e054472854600c702a2204
waelprogamer2025-oss <wael.pro.gamer2025@gmail.com>
2026-07-25 14:19:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,35 @@
 const mineflayer = require('mineflayer');
 
-const bot = mineflayer.createBot({
-  host: 'zxzc.aternos.me',
-  port: 61260,
-  username: 'Bot123',
-  version: '1.21.8'
-});
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'zxzc.aternos.me',
+    port: 61260,
+    username: 'Bot123',
+    version: '1.21.8'
+  });
 
-bot.on('spawn', () => {
-  console.log('Bot joined');
+  bot.on('spawn', () => {
+    console.log('Bot joined');
 
-  setInterval(() => {
-    bot.setControlState('forward', true);
+    setInterval(() => {
+      bot.setControlState('forward', true);
 
-    setTimeout(() => {
-      bot.setControlState('forward', false);
-      bot.look(Math.random() * Math.PI * 2, 0, true);
-    }, 3000);
-  }, 30000);
-});
+      setTimeout(() => {
+        bot.setControlState('forward', false);
+        bot.look(Math.random() * Math.PI * 2, 0, true);
+      }, 3000);
 
-bot.on('error', console.log);
+    }, 30000);
+  });
 
-bot.on('end', () => {
-  console.log('Disconnected. Reconnecting...');
-  setTimeout(() => {
-    require('./bot');
-  }, 10000);
-});
+  bot.on('error', (err) => {
+    console.log('Error:', err.message);
+  });
+
+  bot.on('end', () => {
+    console.log('Disconnected. Reconnecting in 10 seconds...');
+    setTimeout(createBot, 10000);
+  });
+}
+
+createBot();
