412205b2e94ed5c520937f3f8316b03341d399ea
waelprogamer2025-oss <wael.pro.gamer2025@gmail.com>
2026-07-25 13:55:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,24 @@
-const mineflayer=require('mineflayer');
-const bot=mineflayer.createBot({
+const mineflayer = require('mineflayer');
+
 const bot = mineflayer.createBot({
   host: 'zxzc.aternos.me',
   port: 61260,
   username: 'Bot123',
   version: '1.21.8'
 });
-bot.on('spawn',()=>{
- console.log('Bot joined');
- setInterval(()=>{
-  bot.setControlState('forward',true);
-  setTimeout(()=>{
-   bot.setControlState('forward',false);
-   bot.look(Math.random()*Math.PI*2,0,true);
-  },3000);
- },30000);
+
+bot.on('spawn', () => {
+  console.log('Bot joined');
+
+  setInterval(() => {
+    bot.setControlState('forward', true);
+
+    setTimeout(() => {
+      bot.setControlState('forward', false);
+      bot.look(Math.random() * Math.PI * 2, 0, true);
+    }, 3000);
+  }, 30000);
 });
-bot.on('error',console.log);
-bot.on('end',()=>console.log('Disconnected'));
+
+bot.on('error', console.log);
+bot.on('end', () => console.log('Disconnected'));
